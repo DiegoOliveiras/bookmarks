958b6036ae438367a72db654966873b8f9dff856
Diego Oliveira Silva <diegoliveiras@live.com>
2017-09-08 13:54:42 -0300

    Creates Bookmark model

---
+++ src/main/java/com/diegoliveiras/bookmarks/model/Bookmark.java
@@ -0,0 +1,42 @@
+package com.diegoliveiras.bookmarks.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Bookmark {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String title;
+	private String comment;
+	private String marking;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getMarking() {
+		return marking;
+	}
+	public void setMarking(String marking) {
+		this.marking = marking;
+	}
+	
+}
