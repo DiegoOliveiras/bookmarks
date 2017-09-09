f633386865dcbea06e9bce7c90afcec918c2832a
Diego Oliveira Silva <diegoliveiras@live.com>
2017-09-09 19:18:01 -0300

    Corrects more errors caused by merging branches

--- src/main/java/com/diegoliveiras/bookmarks/controller/TitleController.java
+++ src/main/java/com/diegoliveiras/bookmarks/controller/TitleController.java
@@ -1,8 +1,13 @@
 package com.diegoliveiras.bookmarks.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.diegoliveiras.bookmarks.model.Title;
 import com.diegoliveiras.bookmarks.repository.Titles;
 
 @Controller
@@ -11,5 +16,36 @@ public class TitleController {
 	
 	@Autowired
 	private Titles titles;
+	
+	@RequestMapping("/new")
+	public String create() {
+		return "NewTitle";
+	}
+	
+	@RequestMapping
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView ("ListTitle");
+		List<Title> allTitles = titles.findAll();
+		
+		mv.addObject("titles", allTitles);
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView save(Title title) {
+		
+		if (titles.save(title) != null) {
+			ModelAndView mv = list();
+			
+			mv.addObject("success", "Title created with success!");
+
+			return mv;
+		}
+		else {
+			ModelAndView mv = new ModelAndView ("NewTitle");
+			mv.addObject("danger", "Bookmark could not be created.");
+			return mv;
+		}		
+	}
 	
 }
