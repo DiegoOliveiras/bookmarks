472124e82557b5a7ac4df2afaa527f026a822aea
Diego Oliveira Silva <diegoliveiras@live.com>
2017-09-09 10:46:31 -0300

    Creates Users Listing and Sign In pages

--- src/main/java/com/diegoliveiras/bookmarks/controller/UserController.java
+++ src/main/java/com/diegoliveiras/bookmarks/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.diegoliveiras.bookmarks.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.diegoliveiras.bookmarks.model.Bookmark;
 import com.diegoliveiras.bookmarks.model.User;
+import com.diegoliveiras.bookmarks.repository.Bookmarks;
 import com.diegoliveiras.bookmarks.repository.Users;
 
 @Controller
@@ -15,17 +19,33 @@ public class UserController {
 	
 	@Autowired
 	private Users users;
+	@Autowired
+	private Bookmarks bookmarks;
 	
 	@RequestMapping("/new")
 	public String create() {
 		return "NewUser";
 	}
 	
+	@RequestMapping
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView ("ListUser");
+		List<User> allUsers = users.findAll();
+		
+		mv.addObject("users", allUsers);
+		return mv;
+	}
+	
+	@RequestMapping("/signin")
+	public String signIn() {
+		return "SignIn";
+	}
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView save(User user) {
 		
 		if (users.save(user) != null) {
-			ModelAndView mv = new ModelAndView ("NewUser");
+			ModelAndView mv = new ModelAndView ("SignIn");
 			
 			mv.addObject("success", "User created with success!");
 
